7e258b0fac3eefe2d28497a54cd79e6c6acfcf8f
Luis Presa Collada <lpbest1777@gmail.com>
2021-10-21 09:13:30 +0200

    Time displacement, same day, day banned constrictions done.

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -13,9 +13,10 @@ import geneticAlgorithm.GeneticCore;
 import geneticAlgorithm.Individual;
 
 import java.io.IOException;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+
+import static java.util.Collections.*;
 
 public class App {
 
@@ -36,7 +37,7 @@ public class App {
         FitnessFunction fn = new FitnessFunctionImpl(dataHandler);
 
         GeneticCore genCore = new GeneticCore(individualPrime, 50);
-        Individual finalOne = genCore.geneticAlgorithm(0.15, fn, 100);
+        Individual finalOne = genCore.geneticAlgorithm(0.15, fn, 1000);
         System.out.println();
 
         CromosomeDecoder decoder = new CromosomeDecoder();
@@ -46,7 +47,7 @@ public class App {
 
         List<Exam> finalResult = dataHandler.getClonedSchedule();
         Comparator<Exam> examComparator = new ExamDatesComparator();
-        Collections.sort(finalResult, examComparator);
+        sort(finalResult, examComparator);
         ExamParser.parseToExcel(finalResult, dataHandler.getConfigurer().getFilePaths("outputFile"));
     }
 }
